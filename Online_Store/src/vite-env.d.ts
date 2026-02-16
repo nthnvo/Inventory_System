e5d146fb842eb5c8007d620f6cc5a39acae199f7
nthnvo <aounatthasorn@gmail.com>
2026-02-16 16:30:39 +0700

    Delete Online_Store directory

--- Online_Store/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-interface ImportMetaEnv {
-  readonly VITE_API_URL: string;
-  // เพิ่มตัวแปรอื่นถ้ามี เช่น:
-  // readonly VITE_API_KEY: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
